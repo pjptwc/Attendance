3d395b47305dc574af2b06641a982dccb871447c
Pratikbhai Patel <pratikpatel@me.com>
2016-03-28 18:35:17 -0400

    Initial Commit

---
+++ Attendence/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Attendence
+//
+//  Created by Pratikbhai Patel on 3/28/16.
+//  Copyright © 2016 Pratikbhai Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
